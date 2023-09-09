27406343722dfcaf474d5103548f9e60fbd8ce40
Facundo Manuel Perez Duco <perezfacundo11@hotmail.com>
2023-09-09 16:39:49 +0200

    correccion de la 3ra entrega coderhouse

--- index.js
+++ index.js
@@ -1,88 +1,89 @@
-
-
-// Función mostrar saludo inicial.
-function saludo() {
+// Función para mostrar un saludo inicial.
+function mostrarSaludo() {
   Swal.fire({
     title: 'Nike Friends',
-    text: 'Bienvenido a Tienda en Línea',
+    text: '¡Bienvenido a Tienda en Línea!',
     imageUrl: './images/java.jpg',
     imageWidth: 400,
     imageHeight: 400,
-    imageAlt: 'imagen nike :D',
+    imageAlt: 'Imagen de Nike',
   });
-  console.log("Saludo de inicio");
 }
-document.addEventListener("DOMContentLoaded", function () {
 
-  saludo();
-});
+// Función para obtener la fecha y hora actual formateada.
+function obtenerFechaHoraActual() {
+  const fechaHoraActual = new Date();
+  const nombresDias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  const diaSemana = fechaHoraActual.getDay();
+  const nombreDia = nombresDias[diaSemana];
+  const dia = fechaHoraActual.getDate();
+  const mes = fechaHoraActual.getMonth() + 1;
+  const año = fechaHoraActual.getFullYear();
+  const hora = fechaHoraActual.getHours();
+  const minutos = fechaHoraActual.getMinutes();
+  const segundos = fechaHoraActual.getSeconds();
+  const fechaHoraFormateada = `${nombreDia}, ${dia}/${mes}/${año} ${hora}:${minutos}:${segundos}`;
+  return fechaHoraFormateada;
+}
+
 
 
 // Carga el arreglo de usuarios registrados
+let usuariosRegistrados = [
+  {
+    usuario: 'facundo',
+    contraseña: 'facundo',
+    direccion: 'san bartolomé 20',
+    dni: '12345678',
+    edad: 29
+  },
+  {
+    usuario: 'a',
+    contraseña: 'a',
+    direccion: 'dirección2',
+    dni: '98765432',
+    edad: 30
+  }
+];
+
 const usuariosRegistradosJSON = localStorage.getItem('usuariosRegistrados');
 if (usuariosRegistradosJSON) {
   usuariosRegistrados = JSON.parse(usuariosRegistradosJSON);
 }
-for (var i = 0; i < usuariosRegistrados.length; i++) {
-  var usuario = usuariosRegistrados[i];
-  console.log('Usuario:', usuario.username);
-  console.log('Contraseña:', usuario.password);
-  console.log('Dirección:', usuario.direccion);
-  console.log('DNI:', usuario.dni);
-  console.log('Edad:', usuario.edad);
 
+// Función para iniciar sesión
+function iniciarSesion(username, contraseña) {
+  const usuarioEncontrado = usuariosRegistrados.find(user => user.usuario === username);
 
+  if (usuarioEncontrado && usuarioEncontrado.contraseña === contraseña) {
+    localStorage.setItem('usuarioActual', username);
+    Swal.fire({
+      icon: 'success',
+      title: 'Inicio de Sesión Exitoso',
+      text: '¡Bienvenido, ' + username + '!',
+    });
+    const perfilBtn = document.getElementById('perfilBtn');
+    perfilBtn.textContent = 'Cerrar Sesión';
+    perfilBtn.removeEventListener('click', mostrarLoginOLogoutPopup);
+    perfilBtn.addEventListener('click', mostrarLoginOLogoutPopup);
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error de inicio de sesión',
+      text: 'Credenciales incorrectas. Inténtalo de nuevo o regístrate.',
+    });
+  }
 }
 
+// Evento emergente de inicio de sesión, registro o cierre de sesión
+document.getElementById('perfilBtn').addEventListener('click', mostrarLoginOLogoutPopup);
 
-
-//funcion para mostrar fecha y hora.
-function fechaHora() {
-  var fechaHoraActual = new Date();
-  var nombresDias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-  var diaSemana = fechaHoraActual.getDay();
-  var nombreDia = nombresDias[diaSemana];
-  var dia = fechaHoraActual.getDate();
-  var mes = fechaHoraActual.getMonth() + 1;
-  var año = fechaHoraActual.getFullYear();
-  var hora = fechaHoraActual.getHours();
-  var minutos = fechaHoraActual.getMinutes();
-  var segundos = fechaHoraActual.getSeconds();
-  var fechaHoraFormateada = `${nombreDia}, ${dia}/${mes}/${año} ${hora}:${minutos}:${segundos}`;
-  return fechaHoraFormateada;}
-var fechaHora = fechaHora();
-console.log(fechaHora);
-  
-
-
-
- //arreglo de usuarios
-var usuariosRegistrados = [{
-  username: 'facundo',
-  password: 'facundo',
-  direccion: 'san bartolome 20',
-  dni: '12345678',
-  edad: 29
-},
-{
-  username: 'a',
-  password: 'a',
-  direccion: 'dirección2',
-  dni: '98765432',
-  edad: 30
-},
-];
-
-
-
-
+// Función para abrir el perfil o cerrar sesión
 function mostrarLoginOLogoutPopup() {
-  const currentUser = localStorage.getItem('currentUser');
+  const usuarioActual = localStorage.getItem('usuarioActual');
   const perfilBtn = document.getElementById('perfilBtn');
-  console.log('currentUser:', currentUser); 
 
-
-  if (currentUser) { // Si hay una sesión abierta
+  if (usuarioActual) { // Si hay una sesión abierta
     Swal.fire({
       title: 'Cerrar Sesión',
       text: '¿Estás seguro de que deseas cerrar sesión?',
@@ -92,27 +93,21 @@ function mostrarLoginOLogoutPopup() {
       cancelButtonText: 'Cancelar',
     }).then((result) => {
       if (result.isConfirmed) {
-        console.log(`Sesión cerrada ${fechaHora}`);
-
+        localStorage.removeItem('usuarioActual');
         Swal.fire({
           icon: 'success',
           title: 'Sesión Cerrada',
           text: 'Tu sesión ha sido cerrada correctamente.',
         });
-
-
-        cerrarSesionYRecargar(); //funcion de cerrar sesion y recarga de pagina
-
-
-    
+        cerrarSesionYRecargar(); // Función para cerrar sesión y recargar la página
       }
     });
   } else {
     Swal.fire({
       title: 'Inicio de Sesión',
       html:
-        '<input id="username" class="swal2-input" placeholder="Usuario">' +
-        '<input id="password" type="password" class="swal2-input" placeholder="Contraseña">',
+        '<input id="usuario" class="swal2-input" placeholder="Usuario">' +
+        '<input id="contraseña" type="password" class="swal2-input" placeholder="Contraseña">',
       focusConfirm: false,
       showCancelButton: true,
       showCloseButton: true,
@@ -120,37 +115,17 @@ function mostrarLoginOLogoutPopup() {
       cancelButtonText: 'Registrarse',
     }).then((result) => {
       if (result.isConfirmed) {
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        const usuarioEncontrado = usuariosRegistrados.find(user => user.username === username);
-
-        if (usuarioEncontrado && usuarioEncontrado.password === password) {
-          localStorage.setItem('currentUser', username);
-          console.log(`Sesión iniciada ${fechaHora}`);
-          Swal.fire({
-            icon: 'success',
-            title: 'Inicio de Sesión Exitoso',
-            text: '¡Bienvenido, ' + username + '!',
-          });
-          perfilBtn.textContent = 'Cerrar Sesión';
-          perfilBtn.removeEventListener('click', mostrarLoginPopup);
-          perfilBtn.addEventListener('click', mostrarLoginOLogoutPopup);
-        } else {
-          Swal.fire({
-            icon: 'error',
-            title: 'Error de inicio de sesión',
-            text: 'Credenciales incorrectas. Inténtalo de nuevo o regístrate.',
-          });
-        }
+        const username = document.getElementById('usuario').value;
+        const contraseña = document.getElementById('contraseña').value;
+        iniciarSesion(username, contraseña);
       } else if (result.dismiss === Swal.DismissReason.close) {
-
+        // El usuario cerró el diálogo
       } else {
-       
         Swal.fire({
           title: 'Registro',
           html:
-            '<input id="newUsername" class="swal2-input" placeholder="Nuevo Usuario">' +
-            '<input id="newPassword" type="password" class="swal2-input" placeholder="Nueva Contraseña">' +
+            '<input id="nuevoUsuario" class="swal2-input" placeholder="Nuevo Usuario">' +
+            '<input id="nuevaContraseña" type="password" class="swal2-input" placeholder="Nueva Contraseña">' +
             '<input id="direccion" class="swal2-input" placeholder="Dirección">' +
             '<input id="dni" class="swal2-input" placeholder="DNI">' +
             '<input id="edad" class="swal2-input" placeholder="Edad">',
@@ -160,23 +135,21 @@ function mostrarLoginOLogoutPopup() {
           cancelButtonText: 'Cancelar',
         }).then((result) => {
           if (result.isConfirmed) {
-            const newUsername = document.getElementById('newUsername').value;
-            const newPassword = document.getElementById('newPassword').value;
-            const newDireccion = document.getElementById('direccion').value;
-            const newDNI = document.getElementById('dni').value;
-            const newEdad = document.getElementById('edad').value;
+            const nuevoUsuario = document.getElementById('nuevoUsuario').value;
+            const nuevaContraseña = document.getElementById('nuevaContraseña').value;
+            const direccion = document.getElementById('direccion').value;
+            const dni = document.getElementById('dni').value;
+            const edad = document.getElementById('edad').value;
 
             usuariosRegistrados.push({
-              username: newUsername,
-              password: newPassword,
-              direccion: newDireccion,
-              dni: newDNI,
-              edad: newEdad,
+              usuario: nuevoUsuario,
+              contraseña: nuevaContraseña,
+              direccion: direccion,
+              dni: dni,
+              edad: edad,
             });
 
-            //nuevo usuario en Local Storage
             localStorage.setItem('usuariosRegistrados', JSON.stringify(usuariosRegistrados));
-            //localStorage.setItem(newUsername, JSON.stringify({ password: newPassword }));
 
             Swal.fire({
               icon: 'success',
@@ -190,47 +163,39 @@ function mostrarLoginOLogoutPopup() {
   }
 }
 
-
-
-// Evento emergente de inicio de sesión, registro o cierre de sesión
-document.getElementById('perfilBtn').addEventListener('click', mostrarLoginOLogoutPopup);
-
-
-
 // Función para cerrar sesión al cargar la página
 function cerrarSesionAlCargar() {
-  const currentUser = localStorage.getItem('currentUser');
+  const usuarioActual = localStorage.getItem('usuarioActual');
   const perfilBtn = document.getElementById('perfilBtn');
 
-  if (currentUser) {
-    localStorage.removeItem('currentUser');
+  if (usuarioActual) {
+    perfilBtn.textContent = 'Cerrar Sesión';
+    perfilBtn.removeEventListener('click', mostrarLoginOLogoutPopup);
+  } else {
     perfilBtn.textContent = 'Iniciar Sesión';
     perfilBtn.removeEventListener('click', mostrarLoginOLogoutPopup);
-    perfilBtn.addEventListener('click', mostrarLoginPopup);
   }
 }
-function cerrarSesionYRecargar() {
-  const currentUser = localStorage.getItem('currentUser');
-  if (currentUser) {
-    localStorage.removeItem('currentUser');
-    location.reload();}}
+
 document.addEventListener("DOMContentLoaded", function () {
-  cerrarSesionAlCargar(); 
-  saludo();
+  cerrarSesionAlCargar();
+  mostrarSaludo();
   document.getElementById('perfilBtn').addEventListener('click', mostrarLoginOLogoutPopup);
 });
- 
+
+// Función para cerrar sesión y recargar la página
+function cerrarSesionYRecargar() {
+  localStorage.removeItem('usuarioActual');
+  location.reload();
+}
 
 
-//boton info 
+// Botón de información
 document.getElementById('infoBtn').addEventListener('click', function () {
-
   Swal.fire({
     title: '3ra Pre Entrega del proyecto para CoderHouse',
-    text: '- Optimizacion del Proyecto- Codificacion de Funciones; Ampliar y refinar flujo de trabajo; Modificacion del Dom; salida de resultdos por html;definir eventos; almacenamiento de datos y recuperacion. ',
+    text: '- Optimización del Proyecto- Codificación de Funciones; Ampliar y refinar flujo de trabajo; Modificación del DOM; salida de resultados por HTML; definir eventos; almacenamiento de datos y recuperación. ',
     icon: 'success',
     confirmButtonText: 'Cerrar'
   });
 });
-
-
